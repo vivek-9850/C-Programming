eb2e6c8c4b5420ffea941e7f5e78dbc5337a8b79
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:28:07 +0530

    Create Function3.c

---
+++ Function3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int Addition(int No1, int No2)
+{
+    int Sum = 0;
+    Sum = No1 + No2;
+    return Sum;
+}
+
+int main()
+{
+    int value1 = 10;
+    int value2 = 11;
+    int Ans = 0;
+
+    Ans = Addition(value1,value2);
+
+    printf("Addition is : %d\n",Ans);
+
+    return 0;
+}
