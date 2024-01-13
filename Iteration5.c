f5255ff59f150678e15db2f10712b8a40b9ae8a2
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:46:46 +0530

    Create Iteration5.c

---
+++ Iteration5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 1;
+    int Count = 0;
+    printf("Enter number : \n");
+    scanf("%d",&Count);
+
+    do
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+    }while(i <= Count);
+
+    return 0;
+}
