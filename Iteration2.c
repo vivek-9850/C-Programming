abcf682f5ffdf41e15bc594845cd2bb0bd0bcf02
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:45:04 +0530

    Create Iteration2.c

---
+++ Iteration2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0;
+    int a = 0;
+    printf("Enter Number : \n");
+    scanf("%d",&a);
+
+    for(i = 1; i <= a; i++ )
+    {
+        printf("Jay ganesh...\n");
+    }
+
+    return 0;
+}
