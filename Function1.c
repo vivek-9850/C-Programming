107036f38adb72089599c9366a64a9846bc1af72
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:27:00 +0530

    Create Function1.c

---
+++ Function1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//Function Defination
+void Display()
+{
+    printf("Inside Display function...\n");
+}
+
+int main()
+{
+    printf("Inside main function...\n");
+
+    Display();       //Function Call
+
+    return 0;
+}
