35985b489d83f8d06505a0f7bc1263afd1670f15
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-15 22:20:42 +0530

    Create Preprocessor.c

---
+++ Preprocessor.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"Marvellous.h"
+
+#define PI 3.14
+#define DOZEN 12
+
+//Entry point function
+int main()
+{
+    struct Demo obj;
+
+    float Value = 10 * 10 * PI;
+    int Count = 10 * DOZEN;
+
+    printf("Value of No : %d\n",No);
+    printf("Area is : %f\n",Value);
+    printf("Count is : %d\n",Count);
+
+    return 0;
+}
