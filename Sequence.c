ffc935d6ddb19f001d1b58fce619fdadf219bbbf
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-15 22:28:05 +0530

    Create Sequence.c

---
+++ Sequence.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay ganesh...\n");
+    printf("Jay ganesh...\n");
+    printf("Jay ganesh...\n");
+    printf("Jay ganesh...\n");
+    printf("Jay ganesh...\n");
+
+    return 0;
+}
